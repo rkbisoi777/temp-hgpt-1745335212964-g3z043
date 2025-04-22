c3a2bee1083ba01e6636576ffdd127aeb7d417d3
rkbisoi777 <52654372+rkbisoi777@users.noreply.github.com>
2025-04-22 20:53:38 +0530

    Copy src/pages/AddProperty.tsx from hgpt

---
+++ src/pages/AddProperty.tsx
@@ -0,0 +1,9 @@
+import { PropertyForm } from '../components/property/PropertyForm';
+
+export function AddProperty() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <PropertyForm />
+    </div>
+  );
+}
