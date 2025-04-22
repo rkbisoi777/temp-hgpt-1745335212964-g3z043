38e866a246458d61f41ce0e0ac535a5b183fbe64
rkbisoi777 <52654372+rkbisoi777@users.noreply.github.com>
2025-04-22 20:52:59 +0530

    Copy src/constants/countryCodes.ts from hgpt

---
+++ src/constants/countryCodes.ts
@@ -0,0 +1,12 @@
+export const countryCodes = [
+  { code: '+91', name: 'IN' }, // India
+  { code: '+1', name: 'USA' },  // USA
+  { code: '+44', name: 'UK' }, // United Kingdom
+  { code: '+81', name: 'JP' }, // Japan
+  { code: '+86', name: 'CN' }, // China
+  { code: '+61', name: 'AU' }, // Australia
+  { code: '+49', name: 'DE' }, // Germany
+  { code: '+33', name: 'FR' }, // France
+  { code: '+7', name: 'RU' },  // Russia
+  { code: '+65', name: 'SG' }, // Singapore
+] as const;
