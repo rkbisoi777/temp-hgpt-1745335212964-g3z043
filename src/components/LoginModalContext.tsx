c12211447ff99983167e8140ae8e31835067c7b6
rkbisoi777 <52654372+rkbisoi777@users.noreply.github.com>
2025-04-22 20:50:49 +0530

    Copy src/components/LoginModalContext.tsx from hgpt

---
+++ src/components/LoginModalContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+
+interface ModalContextType {
+  isLoginModalOpen: boolean;
+  openLoginModal: () => void;
+  closeLoginModal: () => void;
+}
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = () => setIsLoginModalOpen(true);
+  const closeLoginModal = () => setIsLoginModalOpen(false);
+
+  return (
+    <ModalContext.Provider value={{ isLoginModalOpen, openLoginModal, closeLoginModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within a ModalProvider");
+  }
+  return context;
+};
